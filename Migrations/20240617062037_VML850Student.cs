731af7e0a3ba34115b96b9cf8ddee73eb16fbb06
mailinh0806 <vumailinh29122002@gmail.com>
2024-06-17 13:34:16 +0700

    Final

---
+++ Migrations/20240617062037_VML850Student.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VMLbaithi2324.Migrations
+{
+    /// <inheritdoc />
+    public partial class VML850Student : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Models/VML850Student.cs
@@ -0,0 +1,8 @@
+namespace VMLbaithi2324.Models
+{
+    public class VML850Student : VML850Person
+    {
+        public int VM850IDStudent { get; set; }
+        public string VML850Ten { get; set; }
+    }
+}
